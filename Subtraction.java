b7a896e44f4e9a817d95e61ae7e686e09ed9164b
Jranae <112411872+Jranae@users.noreply.github.com>
2022-08-29 13:11:52 -0400

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction
+{
+public static void main (String[] args)
+{
+int number1= 3;
+int number2= 5;
+int subtraction= number1 - number2;
+System.out.println(subtraction);
+}
+}
