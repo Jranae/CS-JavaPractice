8928e99f6012e77e864760746e6cc1e03d844119
Jranae <112411872+Jranae@users.noreply.github.com>
2022-08-29 13:11:34 -0400

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+public class Multiplication
+{
+public static void main (String[] args)
+{
+int number1= 3;
+int number2= 5;
+int multiplication= number1 * number2;
+System.out.println(multiplication);
+}
+}
