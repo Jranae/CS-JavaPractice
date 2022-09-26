1f7f9b9547a0d020a01d8a38267447cf7cb4f468
Jranae <112411872+Jranae@users.noreply.github.com>
2022-09-26 11:48:02 -0400

    Add files via upload

---
+++ EnumPractice.java
@@ -0,0 +1,19 @@
+enum Color
+{ Red, Green, Yellow, Blue, Orange, White; 
+}
+public class EnumPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Color c1 = Color.Red;
+		System.out.println(c1);
+		System.out.println(c1.ordinal());
+		
+		Color c2 = Color.Yellow;
+		System.out.println(c2);
+		System.out.println(c2.ordinal());
+		
+	}
+
+}
